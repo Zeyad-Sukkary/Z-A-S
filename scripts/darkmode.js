b33bc432c1b302abbab0550af4c6b250c1c034f7
Zeyad El-Sukkary <zeyadsukkary@gmail.com>
2025-12-30 17:12:10 +0300

    Implement dark mode toggle with localStorage support
    Add functionality to toggle dark mode and save preference

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,1 +1,17 @@
 
+window.onload = function() {
+  if (localStorage.getItem('theme') === 'dark') {
+      document.body.classList.add('darkmode');
+  }
+};
+
+document.getElementById('theme-switch').addEventListener('click', function () {
+  document.body.classList.toggle('darkmode');
+  // Optional: Save the theme preference in localStorage
+  if (document.body.classList.contains('darkmode')) {
+      localStorage.setItem('theme', 'dark');
+  } else {
+      localStorage.setItem('theme', 'light');
+  }
+});
+
